bdc1272552a1e4dacb885d831192fa19fd058f20
Erth616 <dburnsx@gmail.com>
2019-10-04 18:07:23 -0400

    99%
    Fix messaging during compression -- then g2g

--- caches.py
+++ caches.py
@@ -1,14 +1,28 @@
 #
 # https://www.geeksforgeeks.org/working-zip-files-python/
-#
+# https://pythonspot.com/tk-file-dialogs/
+# https://pypi.org/project/py2exe/
+# https://stackoverflow.com/questions/41578808/python-indexerror-tuple-index-out-of-range-when-using-py2exe
+# https://stackoverflow.com/questions/1406145/how-do-i-get-rid-of-python-tkinter-root-window
+# https://thispointer.com/python-how-to-move-files-and-directories/
+# 
+# 
+
 import os
 import zipfile
 from tkinter import filedialog
 from tkinter import *
+import shutil
+import subprocess
+import platform
+from tkinter import messagebox
+
 
 #GUI for directory selection
 root = Tk()
-root.filename =  filedialog.askdirectory(initialdir = "/",title = "Select Cache Directory")
+root.withdraw() #hide the root window
+
+root.filename =  filedialog.askdirectory(initialdir = "/",title = "Please Select Cache Directory")
 path = (root.filename)
 
 #zip the directory with the caches
@@ -19,6 +33,19 @@ def zipdir(path, ziphandle):
             ziphandle.write(os.path.join(root, file))
 
 if __name__ == '__main__':
+    #currently requires user input to continue -- fix this
+    messagebox.showinfo("Compressing", "Please wait while the caches are compressed.") 
+
     zipf = zipfile.ZipFile('caches.zip', 'w', zipfile.ZIP_DEFLATED)
     zipdir(path, zipf)
     zipf.close()
+
+#GUI to get output directory of zipfile
+root.filename =  filedialog.askdirectory(initialdir = "/",title = "Please Select Target location to Extract to")
+tpath = (root.filename)
+
+#move zip file to target location
+shutil.move("caches.zip", tpath)
+
+#openFilelocation of zipped files
+os.startfile(tpath)
