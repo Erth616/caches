9d48f56e64ed9d44a46aafc908396f1526f1994d
Erth616 <dburnsx@gmail.com>
2019-10-04 20:46:23 -0400

    display messages continued
    while loop for display during compression

--- caches.py
+++ caches.py
@@ -7,7 +7,6 @@
 # https://thispointer.com/python-how-to-move-files-and-directories/
 # 
 # 
-
 import os
 import zipfile
 from tkinter import filedialog
@@ -16,7 +15,7 @@ import shutil
 import subprocess
 import platform
 from tkinter import messagebox
-
+from pymsgbox import *
 
 #GUI for directory selection
 root = Tk()
@@ -25,20 +24,29 @@ root.withdraw() #hide the root window
 root.filename =  filedialog.askdirectory(initialdir = "/",title = "Please Select Cache Directory")
 path = (root.filename)
 
+i = 0
 #zip the directory with the caches
 def zipdir(path, ziphandle):
     
+
     for root, directories, files in os.walk(path):
         for file in files:
             ziphandle.write(os.path.join(root, file))
 
 if __name__ == '__main__':
-    #currently requires user input to continue -- fix this
-    messagebox.showinfo("Compressing", "Please wait while the caches are compressed.") 
+    while i==0:
+        #show message box
+        alert(text='Please Wait while the files are compressed...', title='Compressing', button='Cancel')
 
-    zipf = zipfile.ZipFile('caches.zip', 'w', zipfile.ZIP_DEFLATED)
-    zipdir(path, zipf)
-    zipf.close()
+        #zip process
+        zipf = zipfile.ZipFile('caches.zip', 'w', zipfile.ZIP_DEFLATED)
+        zipdir(path, zipf)
+        zipf.close()
+
+        #kill message box
+        i = 1
+    print ("compression complete")
+
 
 #GUI to get output directory of zipfile
 root.filename =  filedialog.askdirectory(initialdir = "/",title = "Please Select Target location to Extract to")
